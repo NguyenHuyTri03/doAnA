10501527689f3493ed84ca62c769fb5b2311905e
NguyenHuyTri <tri.nh12825@sinhvien.hoasen.edu.vn>
2025-05-08 01:22:35 +0700

    Added client

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import Navigation from './customer/components/Navigation/Navigation.jsx';
+import Product from './customer/components/product/Product.jsx';
+import HomePage from './customer/pages/HomePage/HomePage';
+import Footer from './customer/components/footer/footer.jsx';
+import ProductDetails from './customer/components/ProductDetails/ProductDetails.jsx'
+import Cart from './customer/components/Cart/Cart.jsx';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Checkout from './customer/components/Checkout/Checkout.jsx';
+
+function App() {
+  return (
+    <div className="">
+      <BrowserRouter>
+        <Navigation />
+        <div>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/products" element={<Product />} />
+            <Route path="/product/:productId" element={<ProductDetails />} />
+            <Route path="/cart" element={<Cart/>} />
+            <Route path="/checkout" element={<Checkout />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
